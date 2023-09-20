519d069e35f3e3beb943616fac2ebc42e29744f0
kjetil jacobsen <kjetil@phoney.no>
2023-09-20 15:51:27 +0000

    #9 add random terraform provider

--- main.tf
+++ main.tf
@@ -1,2 +1,21 @@
-# This is my first change, Gitpod!
-# This is my second change, Github
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+  }
+}
+
+provider "random" {
+  # Configuration options
+}
+
+resource "random_string" "bucket_name" {
+  length           = 16
+  special          = false
+}
+
+output "random_bucket_name" {
+  value = random_string.bucket_name.result
+}
