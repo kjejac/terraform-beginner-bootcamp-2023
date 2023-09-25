79e11452c92132f821d1d499359376e5ddebe751
kjejac <77548406+kjejac@users.noreply.github.com>
2023-09-25 22:46:12 +0200

    #24 bucket should no longer use random provider (#25)

--- providers.tf
+++ providers.tf
@@ -6,10 +6,10 @@ terraform {
 #    }
 #  }
   required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
+#    random = {
+#      source = "hashicorp/random"
+#      version = "3.5.1"
+#    }
     aws = {
       source = "hashicorp/aws"
       version = "5.17.0"
@@ -21,6 +21,6 @@ provider "aws" {
   # Configuration options
 }
 
-provider "random" {
-  # Configuration options
-}
+#provider "random" {
+#  # Configuration options
+#}
